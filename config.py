12828701ad458907aab33311e988294ccae8731d
Nisar <justnisar@gmail.com>
2015-11-12 23:35:10 -0700

    Added configuration file

---
+++ config.py
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+# The access_token and access_secret are the same. These keys need to be replaced with your twitter developer keys
+
+
+
+consumer_key = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+consumer_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+
+
+
+
+
